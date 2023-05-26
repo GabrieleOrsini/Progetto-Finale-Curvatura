d1de0e7387d6796d1c06bda1e3cdeca625903498
GabrieleOrsini <131452582+GabrieleOrsini@users.noreply.github.com>
2023-05-26 13:16:25 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 function preload(){
   treeModel = loadAsset("albero.glb")
+  burnedTree = loadAsset("rock_and_burned_tree.glb")
+  cherryTree = loadAsset("cherry_tree.glb")
 
 
 }
@@ -13,15 +15,20 @@ function setup() {
   
   var lato= 100;
   var lato2= 100;
-  var numeroAlberi = 50;
+  var numeroAlberi = 10;
   
   for (var i=0; i<=numeroAlberi; i++){
     
     var x = random(-lato/2, lato/2);
     var y = 0;
     var z = random(-lato/2, lato/2);
+
+    var a = random(-lato/2, lato/2);
+    var b = 0;
+    var c = random(-lato/2, lato/2);
     
-    clone(treeModel, x, y, z);
+    clone(cherryTree, x, y ,z);
+    clone(burnedTree, a, b, c);
   }
   
 
